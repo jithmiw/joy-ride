6e149adf62f79e22143c88b8f4f86ed2789626dc
jithmiw <wjithmiravihari@gmail.com>
2025-02-16 18:29:21 +0530

    update the content with exception handler

--- backend/src/main/java/lk/joyride/WebAppInitializer.java
+++ backend/src/main/java/lk/joyride/WebAppInitializer.java
@@ -1,1 +1,32 @@
+package lk.joyride;
 
+import lk.joyride.config.WebAppConfig;
+import lk.joyride.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+
+    // set MultipartConfigElement
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        registration.setMultipartConfig(new MultipartConfigElement(System.getProperty("java.io.tmpdir")));
+    }
+}
---
+++ backend/src/main/java/lk/joyride/util/ResponseUtil.java
@@ -0,0 +1,18 @@
+package lk.joyride.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class ResponseUtil {
+
+    private int status;
+    private String message;
+    private Object data;
+}
+
